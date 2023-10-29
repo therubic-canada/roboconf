9fc1151383fb06628b400740fbc6378218c4ecdd
augustelalande <auguste.lalande@gmail.com>
2023-10-29 01:44:44 -0400

    resolve path before traversal

--- roboconf/config_loader.py
+++ roboconf/config_loader.py
@@ -3,7 +3,7 @@ import yaml
 
 
 def load_nearest_conf(path, verbose=False):
-    current_path = Path(path)
+    current_path = Path(path).resolve()
     if current_path.is_file():
         current_path = current_path.parent
 

248a4b8d8e3f1334ab1fce9b9add538edb27fbd7
augustelalande <auguste.lalande@gmail.com>
2023-11-09 19:43:41 -0500

    bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ package_name = 'roboconf'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     install_requires=['setuptools', 'pyyaml'],
     zip_safe=True,
